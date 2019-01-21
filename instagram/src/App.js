04490741e5812e5c587b27adbdd602b7a2430db0
McNultyPT <mcnultypt@gmail.com>
2019-01-21 10:02:32 -0800

    App.js complete

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postData: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      postData: dummyData
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        {this.state.postData.map((data, index) => {
+          return <PostContainer
+            username={data.username}
+            thumbnailUrl={data.thumbnailUrl}
+            imageUrl={data.imageUrl}
+            likes={data.likes}
+            timestamp={data.timestamp}
+            comments={data.comments}
+            key={index}
+        />
+        })}
       </div>
     );
   }
