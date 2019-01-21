2eecc6e6d3e00c5c4284619c3e5c350c44093a50
McNultyPT <mcnultypt@gmail.com>
2019-01-21 12:10:18 -0800

    PostContainer rendered

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -6,7 +6,8 @@ class CommentSection extends React.Component {
     constructor(props) {
         super(props);
         this.state= {
-            commentArr: []
+            commentArr: [],
+            likes: props.likes
         }
     }
 
@@ -19,14 +20,31 @@ class CommentSection extends React.Component {
     render() {
         return (
            <div>
-               {this.state.commentArr.map((data, index) => {
-                   return <Comment
-                        comments={data.comments}
-                        username={data.username}
-                        text={data.text}
-                        key={index}
-                    />
-               })}
+               <div>
+                    <i class="far fa-heart fa-lg" onClick={this.addNewLike}></i>
+                    <i class="far fa-comment fa-lg"></i>    
+                    <div>
+                        <h3>{this.state.likes}</h3>
+                        <p>likes</p>
+                    </div>
+               </div>
+               <div>
+                    {this.state.commentArr.map((data, index) => {
+                        return <Comment
+                            comments={data.comments}
+                            username={data.username}
+                            text={data.text}
+                            key={index}
+                        />
+                    })}
+                <p>{this.props.timestamp}</p>
+               </div>
+               <form>
+                   <input
+                        type='text'
+                        placeholder='Add a comment...'
+                   />
+               </form>
            </div>
         );
     }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import CommentSection from '../CommentSection/CommentSection';
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
 function PostContainer(props) {
     return (
-        <CommentSection
-            likes={props.likes}
-            comments={props.comments}
-            timestamp={props.timestamp}
-        />
+        <div>
+            <div className='topContent'>
+                <img src={props.thumbnailUrl} />
+                <h3>{props.username}</h3>
+            </div>
+            <div className='midContent'>
+                <img src={props.imageUrl} />
+            </div>
+            <CommentSection
+                likes={props.likes}
+                comments={props.comments}
+                timestamp={props.timestamp}
+            />
+        </div>
     )
 }
 
